a4f3e7a10388a21cd10740c0bc6cff64ffb003cb
Parker Bossier <parkerbossier@gmail.com>
2012-02-15 17:31:02 -0600

    regalah update

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -83,11 +83,11 @@
                         Each sphere is a left/right graphical equalizer. The length of the red, green, and blue bars represents the mean value of the signal buffer.
                         I plan on making the camera rotate around the scene and having the spheres rotate to the beat. If you check out the source code, some of the camera
                         functionality is already there. I just need to flesh it out more when I have the time.
-
-                        <script type="text/javascript" src="http://www.java.com/js/deployJava.js"></script>
                     </p>
 
-                    <div id="sphere_container"></div>
+                    <div id="sphere_container">
+                        <script type="text/javascript" src="http://www.java.com/js/deployJava.js"></script>
+                    </div>
                 </div>
 
                 <div id="flash_page" style="display: none;" class="page">

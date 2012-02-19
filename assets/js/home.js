aef339ffc35bd77ea451dd682c9e2505cad79c96
Parker Bossier <parkerbossier@gmail.com>
2012-02-19 00:35:02 -0600

    regalah update

--- assets/js/home.js
+++ assets/js/home.js
@@ -35,7 +35,7 @@ function processing_page_setup() {
         archive: 'Sphere.jar,jl1.0.jar,jsminim.jar,minim-spi.jar,minim.jar,mp3spi1.9.4.jar,tritonus_aos.jar,tritonus_share.jar,core.jar',
         width: 800,
         height: 650,
-        codebase: '/assets/sphere/'
+        codebase: 'http://dev.parkerbossier.comassets/sphere/'
     };
     var parameters = { 
         image: 'loading.gif',

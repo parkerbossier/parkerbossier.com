a0b5923d8dc3aa5b7a5fda53cdf027b65d845723
Parker Bossier <parkerbossier@gmail.com>
2012-02-18 18:06:34 -0600

    regalah update

--- application/controllers/resume.php
+++ application/controllers/resume.php
@@ -12,7 +12,13 @@ class Resume extends CI_Controller {
             </head>
 
             <body>
-                <iframe src="/assets/pdfs/resume.pdf" style="width: 100%; height: 100%" frameborder="0"/>
+                <object data="myfile.pdf" type="application/pdf" width="100%" height="100%">
+                    <p>
+                        It appears you don't have a PDF plugin for this browser.
+                        No big deal. You can <a href="/assets/pdfs/resume.pdf">click here</a>
+                        to download the PDF.</a>
+                    </p>
+                </object>
             </body>
         </html>
         <?php

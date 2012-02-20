bd58b1b51fceb01619544cda343d4d5c60583f69
Parker Bossier <parkerbossier@gmail.com>
2012-02-19 23:10:02 -0600

    regalah update

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -99,17 +99,7 @@
                         just have to refresh :/
                     </p>
 
-                    <div id="sphere_container">
-                        <applet id="sphere"
-                                code="Sphere.class"
-                                archive="Sphere.jar,jl1.0.jar,jsminim.jar,minim-spi.jar,minim.jar,mp3spi1.9.4.jar,tritonus_aos.jar,tritonus_share.jar,core.jar"
-                                width="800"
-                                height="650">
-                            <param name="image" value="loading.gif">
-                            <param name="centerimage" value="true">
-<!--                            <param name="codebase_lookup" value="false">-->
-                        </applet>
-                    </div>
+                    <div id="sphere_container"></div>
                 </div>
 
                 <div id="flash_page" style="display: none;" class="page">

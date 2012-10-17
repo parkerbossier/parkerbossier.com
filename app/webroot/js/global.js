be304f00ba73523aff6a063e5808cfe738aa1b0b
Parker Bossier <parkerbossier@gmail.com>
2012-10-17 15:19:22 -0400

    Rotating cards update

---
+++ app/webroot/js/global.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('.card').addClass('rotate-in');
+	
+	// only intercept links if webkit or firefox
+	if (/mozilla/i.test(navigator.userAgent) || /webkit/i.test(navigator.userAgent)) {
+		// intercept link operation to allow for transition
+		$('.navbar a[target!="_blank"]').click(function(e) {
+			e.preventDefault();
+				
+			// disallow clicking on an active link
+			if (!$(this).parent().hasClass('active')) {
+				$('.card').removeClass('rotate-in').addClass('rotate-out');
+				var addr = $(this).attr('href');
+				setTimeout(function() {
+					window.location.href = addr;
+				}, 500);
+			}
+		});
+	}
+});

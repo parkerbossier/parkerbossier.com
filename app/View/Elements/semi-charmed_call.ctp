960b6fcfed4aa3175262686cf17951752de9828a
Parker Bossier <parkerbossier@gmail.com>
2012-12-10 01:21:09 -0500

    added kinetic typography

---
+++ app/View/Elements/semi-charmed_call.ctp
@@ -0,0 +1,21 @@
+<div class="semi-charmed-call content-item" style="display: none;">
+    <div class="row-fluid centered-text">
+        <h2>Kinetic Typography (Semi-Charmed Call)</h2>
+    </div>
+
+    <div class="row-fluid">
+        <div class="span12 centered-text embedded-video-parent">
+            <iframe width="560" height="315" src="http://www.youtube.com/embed/Up-BZHaE6nY" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <br/>
+
+    <div class="row-fluid">
+        <div class="span10 offset1">
+            <p>
+                This video was for a class project at CMU. It was my first stab at kinetic typography and using After Effects CS6. I've only completed the first half of the video, but I'll be updating it when I have the time. Visit the <a href="http://www.youtube.com/watch?v=Up-BZHaE6nY" target="_blank">description on YouTube</a> for the original song.
+            </p>
+        </div>
+    </div>
+</div>
--- app/webroot/js/portfolio.js
+++ app/webroot/js/portfolio.js
@@ -1,17 +1,24 @@
 $(function() {
-	// pills click handler (show the correct content)
-	$('.nav-pills li').not('.disabled').not('active').click(function() {
-		var self = $(this);
-		var divToShow = self.children('a').data('name');
-		self.siblings().removeClass('active');
-		self.addClass('active');
-		$('#content-container .content-item:visible').hide();
-		$('.' + divToShow).show();
-	});
+    // pills click handler (show the correct content)
+    $('.nav-pills li').not('.disabled').not('active').click(function() {
+        var self = $(this);
+        var divToShow = self.children('a').data('name');
+        self.siblings().removeClass('active');
+        self.addClass('active');
+        $('#content-container .content-item:visible').hide();
+        $('.' + divToShow).show();
+    });
+
+    // runs after the rotate transition (not needed for firefox)
+    $('.card').bind('webkitTransitionEnd oTransitionEnd otransitionend msTransitionEnd', function(){
+        // reset the HTML to make the video work correctly
+        var tempHTML = $('.embedded-video-parent').html();
+        $('.embedded-video-parent').html(tempHTML);
+    });
 });
 
 // select the correct tab on window load
 $(window).load(function() {
-	var hash = window.location.hash.substr(1) || false;
-	if (hash) $('.nav-stacked a[data-name="' + hash + '"]').parent().click();
+    var hash = window.location.hash.substr(1) || false;
+    if (hash) $('.nav-stacked a[data-name="' + hash + '"]').parent().click();
 });

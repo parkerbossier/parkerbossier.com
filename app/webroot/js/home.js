3aac7e5f6b2e721c3f4fa02fb077c116c79924f0
Parker Bossier <parkerbossier@gmail.com>
2012-12-10 01:21:09 -0500

    added kinetic typography

--- app/webroot/js/home.js
+++ app/webroot/js/home.js
@@ -1,15 +1,15 @@
 // moves the intro text to the vertical center of the row
 $(window).bind('load', valign_site_intro);
 $(window).resize(function() {
-	valign_site_intro();
+    valign_site_intro();
 });
 
 function valign_site_intro() {
-	// don't adjust offset if the row isn't split
-	if ($('#site-intro').css('float') != 'none') {
-		var parent_height = $('#site-intro').parent().height();
-		var intro_height = $('#site-intro').height();
-		var offset = (parent_height-intro_height)/2;
-		$('#site-intro').css('margin-top', offset + 'px');
-	}
+    // don't adjust offset if the row isn't split
+    if ($('#site-intro').css('float') != 'none') {
+        var parent_height = $('#site-intro').parent().height();
+        var intro_height = $('#site-intro').height();
+        var offset = (parent_height-intro_height)/2;
+        $('#site-intro').css('margin-top', offset + 'px');
+    }
 }

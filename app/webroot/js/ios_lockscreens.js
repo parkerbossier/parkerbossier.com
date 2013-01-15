4e0add6152e18320b0704cd1881a241282a0ba09
Parker Bossier <parkerbossier@gmail.com>
2013-01-15 14:44:52 -0500

    all donegit add .?

--- app/webroot/js/ios_lockscreens.js
+++ app/webroot/js/ios_lockscreens.js
@@ -1,5 +1,10 @@
 $(function() {
     // make the description divs equal heights
-    var $sameHeights = $('.same-height');
-    $sameHeights.height(Math.max($($sameHeights[0]).height(), $($sameHeights[1]).height()));
+    var sameHeight = function() {
+        var $sameHeights = $('.same-height').height('');
+        $sameHeights.height(Math.max($($sameHeights[0]).height(), $($sameHeights[1]).height()));
+    }
+
+    $(window).on('resize', sameHeight);
+    sameHeight();
 });

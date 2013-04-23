c141539e38dca8bb8ff0683496b65adc3c13033d
Parker Bossier <parkerbossier@gmail.com>
2013-04-23 10:52:00 -0400

    jshbef

--- app/Controller/ElectricImpController.php
+++ app/Controller/ElectricImpController.php
@@ -7,10 +7,6 @@ class ElectricImpController extends AppController {
     public function recordRfid() {
         $this->autoRender = false;
 
-        pre_var_dump($this->request->query['value']);
-        $json = json_decode($this->request->query['value']);
-        pre_var_dump($json);
-
         $this->Rfid->save(array(
             'Rfid' => array(
                 'value' => $this->request->query['value']

d6169e9065914337956250c42cbd542c75b68c90
Parker Bossier <parkerbossier@gmail.com>
2013-06-07 11:24:23 -0400

    testing newrelic

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,5 +6,10 @@ class HomeController extends AppController {
 
     }
 
+    public function foo() {
+        $this->autoRender = false;
+        echo newrelic_get_browser_timing_header();
+    }
+
 }
 

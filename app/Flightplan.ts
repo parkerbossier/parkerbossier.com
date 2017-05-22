955f7f81c52bd67f073c448a2a1bf96678e4ce3b
Parker Bossier <parkerbossier@gmail.com>
2017-05-22 08:29:24 -0700

    About to go React

--- app/Flightplan.ts
+++ app/Flightplan.ts
@@ -1,18 +1,30 @@
 import './Flightplan.less';
 
 export class Flightplan {
+	private startTime = new Date().getTime();
+
 	constructor(private element: JQuery) {
 		const animateClass = 'Flightplan-me--animateFocus';
+		const mars = element.find('.Flightplan-mars');
 		const me = element.find('.Flightplan-me');
 		const outerRing = element.find('.Flightplan-meRingOuter');
 
+		// restart the ring transition when it finishes
 		outerRing.on('animationend', () => {
-			console.log('reset!');
-
 			me.removeClass(animateClass);
 			setTimeout(() => {
 				me.addClass(animateClass);
 			}, 0);
-		})
+		});
+
+		const foo = () => {
+			const diff = new Date().getTime() - this.startTime;
+			const percent = diff / 50;
+			mars.css('background-position', `${percent}% 100%`);
+
+			requestAnimationFrame(foo);
+		}
+
+		//foo();
 	}
 }
